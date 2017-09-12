880704a46cb102b87de6bbf55c4e312e637fcaa3
Unknown <michael_grodkowski@yahoo.com>
2017-09-12 11:09:14 -0600

    Updates to unity guesser game

---
+++ UnityPRJCT/Assets/UnityPRJCT.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityPRJCT: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number UnityPRJCTc#");
+		print ("Pick a number in your head");
+		print ("The highest number you can pick is 100");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
