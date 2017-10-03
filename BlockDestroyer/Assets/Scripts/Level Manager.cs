3747f685d31f6280cf5665a241591f0921f8e646
Unknown <michael_grodkowski@yahoo.com>
2017-10-03 11:13:31 -0600

    Block Destroyer

---
+++ BlockDestroyer/Assets/Scripts/Level Manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void ExitGame (){
+		print("Tried to Exit Application");
+		Application.Quit ();
+		
+ }
+}
