bc963c452d7308e65ffa359ea324d7aa395f056b
Unknown <michael_grodkowski@yahoo.com>
2017-10-05 11:11:49 -0600

    Block Breaker Projects - update lvl1

--- BlockDestroyer/Assets/Scripts/LevelManager.cs
+++ BlockDestroyer/Assets/Scripts/LevelManager.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
+
+	public void Levelload (string level01){
+		SceneManager.LoadScene (level01);
+	}
+
+
+
 
 	public void ExitGame (){
 		print("Tried to Exit Application");
