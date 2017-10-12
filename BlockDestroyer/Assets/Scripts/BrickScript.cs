6752e158ce5b72ec946d1ddbe18ae672bbd58106
Unknown <michael_grodkowski@yahoo.com>
2017-10-12 11:16:54 -0600

    creating bricks in the block destroyer

---
+++ BlockDestroyer/Assets/Scripts/BrickScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour {
+
+	public int health; 
+
+
+
+
+
+	public class Health : MonoBehaviour 
+	{
+		public const int maxHealth = 3;
+		public int currentHealth = maxHealth;
+
+		if (currentHealth <= 0)
+		{
+				currentHealth = 0;
+				Debug.Log("Dead!");
+
+		}	
+	}
+		// Use this for initialization
+	public.class
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+	
