e458fc11e5addc455ece846c9d775d6665ae564a
Unknown <michael_grodkowski@yahoo.com>
2017-10-24 11:23:56 -0600

    brick breaker - bouncing the ball into blocks

--- BlockDestroyer/Assets/Scripts/BrickScript.cs
+++ BlockDestroyer/Assets/Scripts/BrickScript.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class BrickScript : MonoBehaviour {
 
 	public int health; 
+	public Sprite[] picture;
+	private int count = 0;
 
 	void OnCollisionEnter2D (Collision2D collider)
 	{
 
 		//Take away 1 health for each impact
 		health--;
+		count++;
 
 		// If health is < 0 destroy the brick.
+	
+
 		if (health <= 0) {
 			Destroy (this.gameObject);
 		}
+		GetComponent<SpriteRenderer>().sprite = picture[count];
+
 	}
 }
---
+++ BlockDestroyer/Assets/Scripts/ballscript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour {
+
+	public GameObject paddle;
+	private bool playing = false;
+	private Vector3 paddleToBallVector; // distance from ball to paddle.
+	private Rigidbody2D rigid;
+
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		rigid = this.GetComponent<Rigidbody2D> ();
+		print (rigid);
+	}
+	
+
+	void Update () { 
+
+		if (!playing) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		
+			// If push start button
+			if (Input.GetMouseButtonDown (0)) {
+				// Ball goes flying off of the paddle. 
+				rigid.velocity = new Vector2 (4,20);
+				// Playing needs to be set to true, playing = true;
+				playing = true;
+
+
+
+				print ("pushed mouse");
+			}
+				
+		
+		
+		}
+
+	
+		
+	}
+}
