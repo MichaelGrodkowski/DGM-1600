1f72a11954e0c16a2e39a5ac5afa0ace625e8e74
Unknown <michael_grodkowski@yahoo.com>
2017-10-26 11:20:04 -0600

    working on brick breaker game over, victory

--- BlockDestroyer/Assets/Scripts/BrickScript.cs
+++ BlockDestroyer/Assets/Scripts/BrickScript.cs
@@ -7,6 +7,11 @@ public class BrickScript : MonoBehaviour {
 	public int health; 
 	public Sprite[] picture;
 	private int count = 0;
+	private LevelManager LevelManager;
+
+	void Start(){
+		 LevelManager = FindObjectOfType<LevelManager> ();
+	}
 
 	void OnCollisionEnter2D (Collision2D collider)
 	{
@@ -17,11 +22,14 @@ public class BrickScript : MonoBehaviour {
 
 		// If health is < 0 destroy the brick.
 	
-
+	
 		if (health <= 0) {
 			Destroy (this.gameObject);
+			LevelManager.brickCount--;
+			LevelManager.CheckBrickCount ();
 		}
 		GetComponent<SpriteRenderer>().sprite = picture[count];
+	
 
 	}
 }
--- BlockDestroyer/Assets/Scripts/LevelManager.cs
+++ BlockDestroyer/Assets/Scripts/LevelManager.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	public static int brickCount;
+
+	void Start(){
+		brickCount = FindObjectsOfType<BrickScript> ().Length;
+		print (brickCount);
+	}
+
+
+
+
 	public void Levelload (string level01){
 		SceneManager.LoadScene (level01);
 	}
@@ -17,4 +27,14 @@ public class LevelManager : MonoBehaviour {
 		Application.Quit ();
 		
  }
-}
+	public void LoadNextLevel (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void CheckBrickCount () {
+		if (brickCount <= 0) {
+			LoadNextLevel ();
+		}
+	}
+
+	}
