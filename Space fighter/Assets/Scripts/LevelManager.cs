c4acf8ac82d7fb2fc23ca9dcdac03000de0c5891
Unknown <michael_grodkowski@yahoo.com>
2017-10-31 11:18:33 -0600

    begin work on space fighter

---
+++ Space fighter/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public static int brickCount;
+
+	void Start(){
+	}
+	public void Levelload (string level01){
+		SceneManager.LoadScene (level01);
+	}
+
+
+
+
+	public void ExitGame (){
+		print("Tried to Exit Application");
+		Application.Quit ();
+		
+ }
+	public void LoadNextLevel (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+
+
+
+	}
---
+++ Space fighter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+
+
+
+	void Start () {
+		rig = GetComponent<Rigidbody>();
+		transform.position 
+
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) { transform.position
+
+		}
+		if (Input.GetKeyDown (KeyCode.A)) {
+		
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+		
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+		
+		}
+
+		//check for button push
+		// print something out
+		// move our player.
+		//down
+		//left
+		//right
+
+
+	}
+}
+
+public Text textObject; 
+public bool gasoline = false;
+public enum States
+
+{
+	start, lost, Car_breakdown0, Car_breakdown1, Abandoned_Mansion, Collapsed_Church, Basement_stair_door, Second_floor_stairs, Creaky_side_door, Clerk_office, Investigate_Crying, Storage_closet,
+	Candle_lit_door, Witch_room, Ignore_crying, Cellar, Victory, Cellar_light, Collapsed_church01,
+};
+public States myState;
+
+
+
+// Use this for initialization
+void Start () {
+
+	myState = States.start;
+
+
+}
+
+// Update is called once per frame
+void Update () {
+
+
+
+
