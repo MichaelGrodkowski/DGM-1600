a5d55b5fe9da23d18fbef25333402147e516aafb
Unknown <michael_grodkowski@yahoo.com>
2017-11-16 10:50:07 -0700

    Adding layers and powerups

---
+++ Space fighter/Assets/Scripts/Powerup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+	//types of upgrades through the enum, for the types of the same file.
+	public enum Type {Shield, Plasma, Speed, SuperUltraMegaHyperLaserBeam
+
+	};
+	public Type Powertype;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+
+		Debug.Log ("We hit a powerup!");
+	}
+
+}
