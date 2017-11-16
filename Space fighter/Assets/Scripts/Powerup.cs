e62367ab90b93c072e30da683a1351a971c02d1a
Unknown <michael_grodkowski@yahoo.com>
2017-11-16 11:23:51 -0700

    Compiler errors, for power ups.

--- Space fighter/Assets/Scripts/Powerup.cs
+++ Space fighter/Assets/Scripts/Powerup.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour {
 	//types of upgrades through the enum, for the types of the same file.
-	public enum Type {Shield, Plasma, Speed, SuperUltraMegaHyperLaserBeam
-
-	};
+	public enum Type {Shield, Plasma, speed, SuperUltraMegaHyperLaserBeam}
 	public Type Powertype;
+	public Sprite [] images;
 
 	// Use this for initialization
 	void Start () {
+		switch (Powertype) {
+		case Type.speed:
+			gameObject.GetComponent<SpriteRenderer> ().sprite = images [0];
+		break;
 		
+		}
 	}
-	
+
+
+
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	void OnTriggerEnter2D(Collider2D other) {
 
+	void OnTriggerEnter2D(Collider2D other){
 		Debug.Log ("We hit a powerup!");
+
+
+		if (Powertype == Type.speed) {
+		}
+
+		case Type.speed:
+		default:
+			break;
+		
+
+		switch (Powertype) {
+			case Type.speed
+		
+			other.GetComponent<PlayerController> ().speed *= 2;
+				break;
+			case Type.Shield
+				break;
+
+
+		Destroy (this.gameObject);
 	}
 
 }
